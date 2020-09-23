51f4b5ad055f125a2ab0542de92c90e3bca48476
deji <ehinlanwo.deji@gmail.com>
2020-09-23 14:26:19 +0200

    Initial commit

---
+++ src/main/java/com/group/mandatoryxpscrum/MandatoryxpscrumApplication.java
@@ -0,0 +1,13 @@
+package com.group.mandatoryxpscrum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MandatoryxpscrumApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MandatoryxpscrumApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/group/mandatoryxpscrum/MandatoryxpscrumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group.mandatoryxpscrum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MandatoryxpscrumApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
