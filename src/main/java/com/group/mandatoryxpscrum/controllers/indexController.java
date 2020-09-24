b9f15974b997a633fffd052ac20116d33d386438
Peter <iw4ffl3z@gmail.com>
2020-09-25 00:04:28 +0200

    minor refactor to front-end to use ActivityServer instead of ActivityHandler

--- src/main/java/com/group/mandatoryxpscrum/controllers/indexController.java
+++ src/main/java/com/group/mandatoryxpscrum/controllers/indexController.java
@@ -1,6 +1,7 @@
 package com.group.mandatoryxpscrum.controllers;
 
-import com.group.mandatoryxpscrum.handlers.ActivityHandler;
+import com.group.mandatoryxpscrum.data.services.ActivityService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,12 +9,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class indexController {
 
-    private ActivityHandler activityHandler = new ActivityHandler();
+    @Autowired
+    ActivityService activityService;
 
     // Get mapping for startpage. Adds list of activities to model object.
     @GetMapping("/")
     public String home(Model model){
-        model.addAttribute("activities", activityHandler.readAll());
+        model.addAttribute("activities", activityService.selectAll());
         return "index";
     }
 
