0eaea5e68bc7e1b84a42cf73938b0c3ad2138ffa
Christian <carl_hassel@hotmail.com>
2020-09-26 16:45:42 +0200

    Button for equipment list is done

--- src/main/java/com/group/mandatoryxpscrum/controllers/indexController.java
+++ src/main/java/com/group/mandatoryxpscrum/controllers/indexController.java
@@ -2,10 +2,14 @@ package com.group.mandatoryxpscrum.controllers;
 
 import com.group.mandatoryxpscrum.data.services.ActivityService;
 import com.group.mandatoryxpscrum.data.services.EquipmentService;
+import com.group.mandatoryxpscrum.models.Activity;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestAttribute;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class indexController {
@@ -19,8 +23,14 @@ public class indexController {
     @GetMapping("/")
     public String home(Model model){
         model.addAttribute("activities", activityService.fetchAll());
-        model.addAttribute("equipment",equipmentService.fetchAll());
-        return "Equipment2";
+        return "index";
+    }
+
+    @GetMapping("/activity/{id}/equipment")
+    public String equipment(Model model, @PathVariable String id) {
+        Activity activity = activityService.fetchById(Integer.parseInt(id));
+        model.addAttribute("equipment", activity.getEquipment());
+        return "equipment";
     }
 
  }
