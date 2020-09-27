ad179d49ed6b2f9aa74fc38ff1d1a3fdd24fd755
Peter <iw4ffl3z@gmail.com>
2020-09-27 16:32:37 +0200

    Equipment er nu en list igen

--- src/main/java/com/group/mandatoryxpscrum/models/Activity.java
+++ src/main/java/com/group/mandatoryxpscrum/models/Activity.java
@@ -1,7 +1,9 @@
 package com.group.mandatoryxpscrum.models;
 
 import javax.persistence.*;
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -45,7 +47,7 @@ public class Activity {
      * hvis ikke fjernes Equipment objektet fra databasen næste gang der bliver kaldt på databasen
      */
     @OneToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL, orphanRemoval = true, mappedBy = "activity")
-    private Set<Equipment> equipment = new HashSet<>();
+    private List<Equipment> equipment = new ArrayList<>();
 
     /** tilføjer et Equipment objekt til listen
      * og linker denne activity til equipment objektet
@@ -65,7 +67,7 @@ public class Activity {
 
     /** Constructors + getters og setters */
     public Activity() {}
-    public Activity(int id, String name, String image, String description, Rules rules, Pricing pricing, Set<Equipment> equipment) {
+    public Activity(int id, String name, String image, String description, Rules rules, Pricing pricing, List<Equipment> equipment) {
         this.id = id;
         this.name = name;
         this.image = image;
@@ -117,7 +119,7 @@ public class Activity {
         this.rules = rules;
     }
 
-    public Set<Equipment> getEquipment() {
+    public List<Equipment> getEquipment() {
         return equipment;
     }
 
@@ -126,12 +128,12 @@ public class Activity {
     the number of available pieces of equipment
     ------------------------------------------------*/
     public Integer availableEquipment() {
-        Set<Equipment> available = equipment.stream()
+        List<Equipment> available = equipment.stream()
                 .filter(Equipment::isAvailable)
-                .collect(Collectors.toSet());
+                .collect(Collectors.toList());
         return available.size();
     }
-    public void setEquipment(Set<Equipment> equipment) {
+    public void setEquipment(List<Equipment> equipment) {
         this.equipment = equipment;
     }
 
@@ -149,7 +151,7 @@ public class Activity {
     }
 
     public Equipment getSpecificEquipment(int id) {
-        for( Equipment e : equipment) {
+        for(Equipment e : equipment) {
            if(e.getId() == id) {
                return e;
            }
