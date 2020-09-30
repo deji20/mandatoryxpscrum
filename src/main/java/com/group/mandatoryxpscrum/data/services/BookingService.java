956f072567b37bcd6e9a82ba622de4e5c162b7b4
deji <ehinlanwo.deji@gmail.com>
2020-09-30 14:13:14 +0200

    added linking table for equipment in booking

--- src/main/java/com/group/mandatoryxpscrum/data/services/BookingService.java
+++ src/main/java/com/group/mandatoryxpscrum/data/services/BookingService.java
@@ -5,14 +5,27 @@ import com.group.mandatoryxpscrum.models.Booking;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 public class BookingService {
     @Autowired
     BookingRepository bookingRepository;
-
     public Booking save(Booking booking){
         return bookingRepository.save(booking);
-   }
+    }
+    public Booking fetchById(int id){
+        return bookingRepository.getOne(id);
+    }
+
+    public List<Booking> fetchAll(){
+        return bookingRepository.findAll();
+    }
+
+    public void delete(int id){
+        bookingRepository.deleteById(id);
+    }
 
     /** Sletter en booking */
    public void delete(Booking booking){
