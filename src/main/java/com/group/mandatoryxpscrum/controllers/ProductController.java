39311787714ae483f53d616c0d9a3dda0b6c7c9e
Peter <iw4ffl3z@gmail.com>
2020-10-07 00:08:47 +0200

    github fix

--- src/main/java/com/group/mandatoryxpscrum/controllers/ProductController.java
+++ src/main/java/com/group/mandatoryxpscrum/controllers/ProductController.java
@@ -9,16 +9,51 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 @Controller
 public class ProductController {
+
+    List<Product> cart = new ArrayList<Product>();
 
     @Autowired
     ProductService productService;
 
     @GetMapping("/products")
     public String index(Model model){
+        model.addAttribute("cart", cart);
         model.addAttribute("products", productService.findAll());
-        return "product/index";
+        return "products/index";
+    }
+
+    @PostMapping("/products/addtocart")
+    public String addToCart(Product product, int amount){
+        for(int i = 0; i < amount; i++){
+            cart.add(product);
+        }
+        return "redirect:/products";
+    }
+
+    @GetMapping("/products/cart")
+    public String viewCart(Model model){
+        model.addAttribute("cart", cart);
+        Map<Product, Integer> cartMap = new HashMap<>();
+        int total = 0;
+        for(Product product : cart) {
+            if(!cartMap.containsKey(product)){
+                cartMap.put(product, 1);
+            }
+            else {
+                cartMap.put(product, cartMap.get(product) + 1);
+            }
+            total = total + product.getPrice();
+        }
+        model.addAttribute("cartMap", cartMap);
+        model.addAttribute("total", total);
+        return "products/cart";
     }
 
     @GetMapping("/manager/products/")
