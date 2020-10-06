12b283088333ef552b30f8d8778e326c30be0e88
Philip Specht <PBSpecht@gmail.com>
2020-10-06 21:12:18 +0200

    User story 15 75% done

--- src/main/java/com/group/mandatoryxpscrum/controllers/ProductController.java
+++ src/main/java/com/group/mandatoryxpscrum/controllers/ProductController.java
@@ -1,10 +1,13 @@
 package com.group.mandatoryxpscrum.controllers;
 
 import com.group.mandatoryxpscrum.data.services.ProductService;
+import com.group.mandatoryxpscrum.models.Product;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class ProductController {
@@ -21,6 +24,14 @@ public class ProductController {
     @GetMapping("/manager/products/")
     public String managerIndex(Model model) {
         model.addAttribute("products", productService.findAll());
+        model.addAttribute("product", new Product());
         return "/manager/products";
     }
+
+    @PostMapping("/manager/products/new")
+    public String addEquipment(@ModelAttribute Product product) {
+        productService.save(product);
+        return "redirect:/manager/products/";
+    }
 }
+
