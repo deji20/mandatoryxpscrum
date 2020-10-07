fac67efb8e3ad50028646155047929b0ae253d84
Bjergu <Bjergu@gmail.com>
2020-10-07 15:16:22 +0200

    Booking list tilføjet Filter er tilføjet

--- src/main/java/com/group/mandatoryxpscrum/data/services/StatisticService.java
+++ src/main/java/com/group/mandatoryxpscrum/data/services/StatisticService.java
@@ -2,10 +2,12 @@ package com.group.mandatoryxpscrum.data.services;
 
 import com.group.mandatoryxpscrum.data.repositories.StatisticRepository;
 import com.group.mandatoryxpscrum.models.Activity;
+import com.group.mandatoryxpscrum.models.Booking;
 import com.group.mandatoryxpscrum.models.Statistic;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.awt.print.Book;
 import java.util.List;
 
 @Service
@@ -14,8 +16,15 @@ public class StatisticService {
     @Autowired
     StatisticRepository statisticRepository;
 
-    public List<Statistic> findAll(){
-        return statisticRepository.findAll();
-    }
-
+//    public List<Booking> findAll(){
+//        return statisticRepository.findAll();
+//    }
+//
+//
+//    public List<Booking> listAll(String keyword){
+//        if (keyword != null){
+//            return statisticRepository.findAll(keyword);
+//        }
+//        return statisticRepository.findAll();
+//    }
 }
