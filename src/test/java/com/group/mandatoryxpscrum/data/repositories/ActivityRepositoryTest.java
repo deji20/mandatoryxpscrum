81e6227af61b6a491ab92e9f724f8747cc8eb455
Peter <iw4ffl3z@gmail.com>
2020-10-07 19:30:04 +0200

    test refactoring 2

--- src/test/java/com/group/mandatoryxpscrum/data/repositories/ActivityRepositoryTest.java
+++ src/test/java/com/group/mandatoryxpscrum/data/repositories/ActivityRepositoryTest.java
@@ -1,22 +1,14 @@
 package com.group.mandatoryxpscrum.data.repositories;
 
 import com.group.mandatoryxpscrum.models.Activity;
-import com.group.mandatoryxpscrum.models.Product;
-import org.junit.Before;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestInstance;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.NoSuchElementException;
-import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
